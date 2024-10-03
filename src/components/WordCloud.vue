e2d85520e239504ea741a170c41c23675821f419
ldduck <125320315+ldduck@users.noreply.github.com>
2024-10-03 16:52:26 +1000

    Attempted word cloud development

--- src/components/WordCloud.vue
+++ src/components/WordCloud.vue
@@ -5,6 +5,54 @@ export default {
   name: 'WordCloud',
   components: {
     Vue3WordCloud
+  },
+  data() {
+    return {
+      theData: {},
+      theArray: [],
+      wordsArray: [],
+      duckArray: [
+        { text: 'duck', weight: 100 },
+        { text: 'mallard', weight: 80 },
+        { text: 'quack', weight: 60 },
+        { text: 'water', weight: 50 },
+        { text: 'fowl', weight: 40 },
+        { text: 'webbed', weight: 30 },
+        { text: 'feathers', weight: 20 },
+        { text: 'wings', weight: 10 }
+      ]
+    }
+  },
+
+  methods: {
+    fetchData() {
+      let query = 'https://api.collection.nfsa.gov.au/search?query=war'
+      console.log(query)
+      fetch(query).then((response) => {
+        response
+          .json()
+          .then((res) => {
+            this.$data.theData = res
+            const results = this.$data.theData
+
+            this.$data.theArray = results.meta.facet.year.buckets
+            this.$data.wordsArray = []
+
+            this.$data.theArray.forEach((element) => {
+              element.text = element['_id']
+              element.weight = element['count']
+              this.$data.wordsArray.push([element.text, element.weight])
+            })
+            console.log(this.$data.wordsArray)
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+      })
+    }
+  },
+  mounted() {
+    this.fetchData()
   }
 }
 </script>
@@ -13,33 +61,39 @@ export default {
   <div class="wordCloudContainer">
     <h1>Word Cloud Moment</h1>
     <h3>If you can see this. I'm connected right!</h3>
-    <vue3-word-cloud
-      id="titles"
-      class="words"
-      style="width: 600px; height: 600px; text-align: center"
-      :words="[
-        { text: 'Vue', weight: 200 },
-        { text: 'React', weight: 110 },
-        { text: 'Angular', weight: 90 },
-        { text: 'Svelte', weight: 70 },
-        { text: 'Ember', weight: 50 },
-        { text: 'More', weight: 40 },
-        { text: 'Heretic', weight: 38 },
-        { text: 'Gregg', weight: 33 },
-        { text: 'Sanity', weight: 15 },
-        { text: 'Tu Shan', weight: 4 }
-      ]"
-      :color="
-        (word) => {
-          const weight = word.weight as number
-          return weight > 80 ? 'DeepPink' : weight > 45 ? 'RoyalBlue' : 'Indigo'
-        }
-      "
-      :font-size="(word) => (word.weight as number) * 0.5"
-      :font-family="'serif'"
-      :spacing="0.35"
-    >
-    </vue3-word-cloud>
+    <div>
+      <vue3-word-cloud
+        id="titles"
+        style="width: 600px; height: 600px"
+        :words="wordsArray"
+        :color="() => '#' + Math.floor(Math.random() * 16777215).toString(16)"
+        :font-family="'serif'"
+        :spacing="0.35"
+      />
+      <h3>Click on a year to see the magic</h3>
+      <div>
+        <ul class="list-v">
+          <li v-for="result in theData.results" :key="result.text">
+            {{ result.title }}
+            {{ result.name }}
+          </li>
+        </ul>
+
+        <button @click="fetchData">Fetch Data</button>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <ul class="list-v">
+      <!-- create a variable called result, 
+      loop through the API results and add a <li> for each result.
+      Use the result variable to access properties like 'title' and 'name' -->
+      <!-- <li v-for="result in theData.results" :key="result.title">
+        {{ result.title }}
+        {{ result.name }}
+      </li> -->
+    </ul>
   </div>
 </template>
 
@@ -61,6 +115,7 @@ h3 {
 
 #titles {
   width: 100%;
+  margin: auto;
   height: 600px;
   background-color: #d7d7d7;
   padding: 20px;
--- src/vue3-word-cloud.d.ts
+++ src/vue3-word-cloud.d.ts
@@ -1,5 +1,4 @@
 declare module 'vue3-word-cloud' {
-  import { DefineComponent } from 'vue'
   const Vue3WordCloud: DefineComponent<{}, {}, any>
   export default Vue3WordCloud
 }
